72b35973d6f0ca21400c2d42d4b6196210b5b5ec
luanaparedes <lua123na@gmail.com>
2022-11-15 22:31:25 -0500

    color js

--- Assets/script.js
+++ Assets/script.js
@@ -1,40 +1,61 @@
 $(document).ready(function () {
-
     let NowMoment = moment().format("MMMM Do YYYY");
     let displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = NowMoment;
     let currentHour = moment().format("HH");
+    createPage()
+   
 })
 
-$(".time-div").each(function () {
-    var timeDiv = $(this).attr("id").split("-")[1];
-    
-    if (currentHour == timeDiv) {
-      $(this).addClass("present");
-      $(this).children(".description").addClass("white-text");
-    } else if (currentHour < timeDiv) {
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    } else if (currentHour > timeDiv) {
-      $(this).removeClass("future");
-      $(this).addClass("past");
+function createPage(){
+    console.log("running")
+    var container = document.getElementsByClassName('container')[0]
+    console.log(container)
+    for(let i=9; i < 18; i++){
+        container.innerHTML+=
+        `<div class="row">
+        <div class="col-1 time-block">${i}:00</div>
+        <textarea class="col-10 text-area"></textarea>
+        <button class="col-1 saveBtn">Save Task</button>
+       </div>`
     }
-  });
+    addSaveFunctionality()
+    blockColors()
+}
 
-$(".saveBtn").click(function (event) {
-    event.preventDefault();
-    var value = $(this).siblings(".time-block").val();
-    var time = $(this).parent().attr("id").split("-")[1];
-    localStorage.setItem(time, value);
-  });
+function blockColors(){
 
-  //retrieves items from local storage and sets them in proper places
-  $("#hour-09 .time-block").val(localStorage.getItem("09"));
-  $("#hour-10 .time-block").val(localStorage.getItem("10"));
-  $("#hour-11 .time-block").val(localStorage.getItem("11"));
-  $("#hour-12 .time-block").val(localStorage.getItem("12"));
-  $("#hour-13 .time-block").val(localStorage.getItem("13"));
-  $("#hour-14 .time-block").val(localStorage.getItem("14"));
-  $("#hour-15 .time-block").val(localStorage.getItem("15"));
-  $("#hour-16 .time-block").val(localStorage.getItem("16"));
-  $("#hour-17 .time-block").val(localStorage.getItem("17"));
+   for (var i = 0; i < time; i++) {
+
+    var presentHour = moment().format("HH:mm");
+    var timeBlock = document.getElementsByClassName('time-block')[i].innerHTML;
+    var time = document.getElementsByClassName('time-block').length;
+
+  if (presentHour < timeBlock) {
+
+    $("textarea").addClass("present");
+    $("textarea").children(".description").addClass("white-text");
+    
+} else if (presentHour == timeBlock) {
+    $("textarea").removeClass("present");
+    $("textarea").addClass("future");
+    
+} else if (presentHour > timeBlock) {
+    $("textarea").removeClass("future");
+    $("textarea").addClass("past");
+    }
+    console.log(timeBlock);
+}
+
+}
+
+function addSaveFunctionality(){
+    var buttons = document.getElementsByClassName("saveBtn")
+    
+    $(buttons).on("click", function () {
+        var task = $('.text-area').val();
+        localStorage.setItem(presentHour, task);
+        console.log("task")
+    });
+   // console.log(task)
+}
