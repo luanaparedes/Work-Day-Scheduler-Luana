8228da3ea5ca46ea5fed0f0f4a0b115c8d3cd91a
luanaparedes <lua123na@gmail.com>
2022-12-31 10:46:30 -0500

    changed html to try and make color change js easier, didnt work

--- Assets/script.js
+++ Assets/script.js
@@ -2,47 +2,66 @@ $(document).ready(function () {
     let NowMoment = moment().format("MMMM Do YYYY");
     let displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = NowMoment;
-    let currentHour = moment().format("HH");
     createPage()
    
 })
 
 function createPage(){
     console.log("running")
-    var container = document.getElementsByClassName('container')[0]
-    console.log(container)
-    for(let i=9; i < 18; i++){
-        container.innerHTML+=
-        `<div class="row">
-        <div class="col-1 time-block">${i}:00</div>
-        <textarea class="col-10 text-area"></textarea>
-        <button class="col-1 saveBtn">Save Task</button>
-       </div>`
-    }
+   // var container = document.getElementsByClassName('container')[0]
+   // console.log(container)
+   // for(let i=9; i < 18; i++){
+  //      container.innerHTML+=
+    //     `<div class="row">
+    //     <div class="col-1 time-block">${i}:00</div>
+    //     <textarea class="col-10 text-area"></textarea>
+    //     <button class="col-1 saveBtn">Save Task</button>
+    //    </div>`
+  //  }
     addSaveFunctionality()
+  //  blockColors();
 }
 
-function blockColors() {
 
-var presentHour = moment().format("HH:mm");
+//function blockColors() {
 
-var timeBlock = parseInt($(this).attr("id").split("time-block")[1]);
- 
-  if (presentHour < timeBlock) {
+const timeBlocks = document.querySelectorAll('.time-block');
+let presentHour = moment().format("hh");
 
-    $("textarea").addClass("present");
-    $("textarea").children(".description").addClass("white-text");
-    
-} else if (presentHour == timeBlock) {
-    $("textarea").removeClass("present");
-    $("textarea").addClass("future");
-    
-} else if (presentHour > timeBlock) {
-    $("textarea").removeClass("future");
-    $("textarea").addClass("past");
+$('.timeBlock').each(function(){
+    var val = parseInt($(this).prop('id'));
+    if(val > presentHour && val < presentHour+6){
+        $(this).css('background-color','Blue');
+    }else if(val < presentHour && val > presentHour-6){
+        $(this).css('background-color','Red');
+    }else if(val === presentHour){
+        $(this).css('background-color','Green');
+    }else{
+        $(this).css('background-color','White');
     }
-    console.log(timeBlock);
-}
+});
+// for (let timeBlock of timeBlocks) {
+    
+    
+//     const timeDisplay = parseInt(timeBlock.getAttribute('time-block'));
+    
+//     if (presentHour == timeDisplay) {
+
+//     $("textarea").addClass("present");
+//     $("textarea").children(".description").addClass("white-text");
+    
+// } else if (presentHour == timeBlock) {
+//     $("textarea").removeClass("present");
+//     $("textarea").addClass("future");
+    
+// } else if (presentHour > timeBlock) {
+//     $("textarea").removeClass("future");
+//     $("textarea").addClass("past");
+//     }
+    
+// }
+// console.log(timeBlocks)
+// }
 
 
 
